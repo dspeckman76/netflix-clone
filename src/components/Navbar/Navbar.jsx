7443bf598040e2b0b5fc8277d8ac282d3fe0a5d2
dspeckman76 <d.speckman76@gmail.com>
2026-01-15 16:18:17 -0600

    Fix null error event

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,25 +7,31 @@ import profile_img from '../../assets/profile_img.png'
 import caret_icon from '../../assets/caret_icon.svg'
 import { logout } from '../../firebase'
 
-
 const Navbar = () => {
-
-  const navRef = useRef();
+  const navRef = useRef(null)
 
   useEffect(() => {
-    window.addEventListener('scroll', () => {
-      if(window.scrollY >= 80) {
+    const handleScroll = () => {
+      if (!navRef.current) return
+
+      if (window.scrollY >= 80) {
         navRef.current.classList.add('nav-dark')
       } else {
         navRef.current.classList.remove('nav-dark')
       }
-    })
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
   }, [])
 
   return (
     <div ref={navRef} className="navbar">
       <div className="navbar-left">
-        <img src={logo} alt="" />
+        <img src={logo} alt="Logo" />
         <ul>
           <li>Home</li>
           <li>TV Shows</li>
@@ -35,16 +41,18 @@ const Navbar = () => {
           <li>Browse by Language</li>
         </ul>
       </div>
+
       <div className="navbar-right">
-        <img src={search_icon} alt="" className="icons" />
+        <img src={search_icon} alt="Search" className="icons" />
         <p>Children</p>
-        <img src={bell_icon} alt="" className="icons" />
+        <img src={bell_icon} alt="Notifications" className="icons" />
+
         <div className="navbar-profile">
-        <img src={profile_img} alt="" className="profile" />
-        <img src={caret_icon} alt="" />
-        <div className="dropdown">
-          <p onClick={() => {logout()}}>Sign Out</p>
-        </div>
+          <img src={profile_img} alt="Profile" className="profile" />
+          <img src={caret_icon} alt="Caret" />
+          <div className="dropdown">
+            <p onClick={logout}>Sign Out</p>
+          </div>
         </div>
       </div>
     </div>
