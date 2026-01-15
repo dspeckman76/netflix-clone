e267a728b3af3256e7739d8655853cfd2c435642
dspeckman76 <d.speckman76@gmail.com>
2026-01-15 17:57:07 -0600

    safety tweak

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -28,7 +28,7 @@ const Player = () => {
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
     .then(res => res.json())
-    .then(res => setApiData(res.results[0]))
+    .then(res => setApiData(res.results[0] ?? {}))
     .catch(err => console.error(err));
   }, [id, options])
   
